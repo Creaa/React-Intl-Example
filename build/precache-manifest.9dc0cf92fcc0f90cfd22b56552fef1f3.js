5146c2abde6b16bfb76913e1f7899f4781e1de9e
Sebastian Kondratowicz <feeverpl@gmail.com>
2019-07-08 00:04:43 +0200

    remove build from .gitigonore, uploading build

---
+++ build/precache-manifest.9dc0cf92fcc0f90cfd22b56552fef1f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2adc60758e4c898319eb361f9516bf78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b154604f50909b83fbe2",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6fc8149fcbebe52f3e6d",
+    "url": "./static/js/2.07151a6c.chunk.js"
+  },
+  {
+    "revision": "f812c1592100d9af97ed",
+    "url": "./static/js/3.6ff0cffc.chunk.js"
+  },
+  {
+    "revision": "1674f074a4773312cfae",
+    "url": "./static/js/4.d657161d.chunk.js"
+  },
+  {
+    "revision": "8bee054aabfbb345d1c2",
+    "url": "./static/js/5.b0cd2bc0.chunk.js"
+  },
+  {
+    "revision": "b154604f50909b83fbe2",
+    "url": "./static/js/main.910ebb9c.chunk.js"
+  },
+  {
+    "revision": "c86861a26d25ecac91d9",
+    "url": "./static/js/runtime~main.5d59f589.js"
+  }
+]);
